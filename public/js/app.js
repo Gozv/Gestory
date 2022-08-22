a5b572e2a5bbab874504c22bbd6ca57a869dabed
Jesus Farias <jesus.fg2630@gmail.com>
2022-08-21 23:00:48 -0300

    Modificaciones de modo oscuro

--- public/js/app.js
+++ public/js/app.js
@@ -91,28 +91,7 @@ function slides(){
 }
     // Slider ↑↑↑
     // Darkmode ↓↓↓
-    const btnSwitch = document.querySelector('#switch');
-
-    btnSwitch.addEventListener('click', () => {
-        document.body.classList.toggle('dark');
-        btnSwitch.classList.toggle('active');
     
-        // Guardamos el modo en localstorage.
-        if(document.body.classList.contains('dark')){
-            localStorage.setItem('dark-mode', 'true');
-        } else {
-            localStorage.setItem('dark-mode', 'false');
-        }
-    });
-    
-    // Obtenemos el modo actual.
-    if(localStorage.getItem('dark-mode') === 'true'){
-        document.body.classList.add('dark');
-        btnSwitch.classList.add('active');
-    } else {
-        document.body.classList.remove('dark');
-        btnSwitch.classList.remove('active');
-    }
    
 //Swiper
   var swiper = new Swiper(".mySwiper", {
@@ -131,3 +110,5 @@ function slides(){
       prevEl: ".swiper-button-prev",
     },
   });
+
+
