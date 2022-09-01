bef6d7d142b4dece3eb78e108f2e337579ca7256
Jesus Farias <jesus.fg2630@gmail.com>
2022-09-01 10:20:10 -0300

    mongo

--- config/db.js
+++ config/db.js
@@ -1,24 +1,9 @@
-//Configuro base de datos
-import Sequelize from "sequelize";
-import dotenv from "dotenv";
-dotenv.config({path: '.env'});
+ import mongoose from "mongoose"; 
 
-//Toma 4 datos
-const db = new Sequelize(process.env.BD_NOMBRE,process.env.BD_USER,process.env.BD_PASSWORD,{
-    host: process.env.BD_HOST,
-    port: process.env.BD_PORT,
-    dialect: 'mysql',
-    define:{
-        timestrap:true
-    },
-    pool:{
-        max:5,
-        min: 0,
-        aquire: 30000,
-        idle: 10000
-    },
-    operatorAlisases:false
-});
 
-export default db;
+const URI = 'mongodb+srv://jesus2630:HR82fmBFzXGtJgjw@cluster0.cth9r9b.mongodb.net/Gestory-Project'
 
+mongoose
+    .connect(URI)
+    .then(() => console.log("Base de datos conectada"))
+    .catch((e) => console.log("Falló la conexión a la base de datos"))
